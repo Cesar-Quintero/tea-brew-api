cb3742cb6c5448c125c6c755dc07e4e9a466fe07
Cesar-Quintero <cesarqdev@gmail.com>
2022-08-25 11:58:22 -0700

    change get req route

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ const teas = {
 }
 
 app.get('/', (request, response)=>{
-    response.sendFile(__dirname + '/index.html')
+    response.sendFile(__dirname + '/public/index.html')
 })
 
 app.get('/api/:name', (request, response)=>{
