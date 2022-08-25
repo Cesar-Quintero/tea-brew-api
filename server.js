5e8bf3e6a13df29386db6aab3f76915dfa967da8
Cesar-Quintero <cesarqdev@gmail.com>
2022-08-25 12:09:11 -0700

    fix route in server

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ const teas = {
 }
 
 app.get('/', (request, response)=>{
-    response.sendFile(__dirname + '/public/index.html')
+    response.sendFile(__dirname + '/public/')
 })
 
 app.get('/api/:name', (request, response)=>{
