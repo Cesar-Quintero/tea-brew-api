5d81326558d3fbbc36163714aadde1beaa1f0904
Cesar-Quintero <cesarqdev@gmail.com>
2022-08-25 13:39:12 -0700

    simplifed repo for api use only

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ const teas = {
 }
 
 app.get('/', (request, response)=>{
-    response.sendFile(__dirname + '/public/')
+    response.sendFile(__dirname + '/index.html')
 })
 
 app.get('/api/:name', (request, response)=>{
